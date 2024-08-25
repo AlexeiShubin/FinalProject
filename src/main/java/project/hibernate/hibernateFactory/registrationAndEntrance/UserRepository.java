ca95d9dfd16aeb5c3e4e8386959fe39e6cdaafbf
AlexeiShubin <154376277+AlexeiShubin@users.noreply.github.com>
2024-08-25 17:50:20 +0300

    Delete src/main/java/project/hibernate/hibernateFactory/registrationAndEntrance/UserRepository.java

--- src/main/java/project/hibernate/hibernateFactory/registrationAndEntrance/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package project.hibernate.hibernateFactory.registrationAndEntrance;
-
-import project.hibernate.hibernateObjectFactory.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
